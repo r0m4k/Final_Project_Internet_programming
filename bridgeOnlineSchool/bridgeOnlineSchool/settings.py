139bcf575285cde0bdb8185819e3f2ef1df3b93d
r0m4k <122051971+r0m4k@users.noreply.github.com>
2025-07-20 15:56:11 +0300

    Update settings.py

--- bridgeOnlineSchool/bridgeOnlineSchool/settings.py
+++ bridgeOnlineSchool/bridgeOnlineSchool/settings.py
@@ -134,7 +134,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # URLS FOR IMAGES
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
