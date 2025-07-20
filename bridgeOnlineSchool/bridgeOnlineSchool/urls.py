e27fc287a4c12f61b83876e83af29b942219cd54
r0m4k <122051971+r0m4k@users.noreply.github.com>
2025-07-20 15:50:42 +0300

    Update urls.py

--- bridgeOnlineSchool/bridgeOnlineSchool/urls.py
+++ bridgeOnlineSchool/bridgeOnlineSchool/urls.py
@@ -28,6 +28,5 @@ urlpatterns = [
 # Media file serving configuration for development environment
 ] 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
